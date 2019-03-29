888b7cbdd314d7651bf723a336a16d82822553ce
Austin Lasseter <austinlasseter@gmail.com>
2019-03-29 00:21:26 -0400

    working on app

---
+++ app/app/main.py
@@ -0,0 +1,52 @@
+import dash
+from dash.dependencies import Input, Output
+import dash_html_components as html
+import dash_core_components as dcc
+from tabs import tab_1, tab_2, tab_3, tab_4
+
+app = dash.Dash()
+
+app.config['suppress_callback_exceptions'] = True
+
+app.layout = html.Div([
+    html.H1('Dash Tabs component demo'),
+    dcc.Tabs(id="tabs-example", value='tab-1-example', children=[
+        dcc.Tab(label='Tab One', value='tab-1-example'),
+        dcc.Tab(label='Tab Two', value='tab-2-example'),
+        dcc.Tab(label='Tab Three', value='tab-3-example'),
+        dcc.Tab(label='Tab Four', value='tab-4-example'),
+
+    ]),
+    html.Div(id='tabs-content-example')
+])
+
+@app.callback(Output('tabs-content-example', 'children'),
+              [Input('tabs-example', 'value')])
+def render_content(tab):
+    if tab == 'tab-1-example':
+        return tab_1.tab_1_layout
+    elif tab == 'tab-2-example':
+        return tab_2.tab_2_layout
+    elif tab == 'tab-3-example':
+        return tab_3.tab_3_layout
+    elif tab == 'tab-4-example':
+        return tab_4.tab_4_layout
+
+# Tab 1 callback
+@app.callback(dash.dependencies.Output('page-1-content', 'children'),
+              [dash.dependencies.Input('page-1-dropdown', 'value')])
+def page_1_dropdown(value):
+    return 'You have selected "{}"'.format(value)
+
+# Tab 2 callback
+@app.callback(Output('page-2-content', 'children'),
+              [Input('page-2-radios', 'value')])
+def page_2_radios(value):
+    return 'You have selected "{}"'.format(value)
+
+app.css.append_css({
+    'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'
+})
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
---
+++ app/tabs/tab_3.py
@@ -0,0 +1,9 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+tab_3_layout = html.Div([
+    html.H1('Page 3'),
+    html.Div(id='page-3-content')
+])
