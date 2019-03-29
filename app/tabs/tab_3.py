c3cd237bd7e8d2875c35d130081bb08556a33557
Austin Lasseter <austinlasseter@gmail.com>
2019-03-29 00:21:56 -0400

    last commit for tonight

--- app/tabs/tab_3.py
+++
@@ -1,9 +0,0 @@
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output
-
-tab_3_layout = html.Div([
-    html.H1('Page 3'),
-    html.Div(id='page-3-content')
-])
--- app/tabs/tab_4.py
+++
@@ -1,9 +0,0 @@
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output
-
-tab_4_layout = html.Div([
-    html.H1('Page 4'),
-    html.Div(id='page-4-content')
-])
